eee3400e6e20502b5a69ffc1c371f4cbeff2afc2
samsep101 <92789300+samsep101@users.noreply.github.com>
2022-02-21 15:53:29 +0500

    ю

--- src/components/AreaProperty.vue
+++ src/components/AreaProperty.vue
@@ -1,43 +1,25 @@
 <template>
-  <div id="fon" v-on:click="doClose"></div>
-  <div id="properties">
-    <div class="cross"></div>
-    <h2 class="h2-properties">properties</h2>
-    <div class="property">
-      <img
-        src="https://avatars.mds.yandex.net/i?id=aba7b672a689330c910372a22922df80-5897285-images-thumbs&n=13"
-        alt=""
-      />
-      <div class="text">
-        <h3>titile</h3>
-        <p>desc</p>
-      </div>
-    </div>
+  <div id="fon" v-on:click="doClose" v-if="area.length && !close"></div>
+  <transition name="slide-fade">
+    <div id="properties" v-if="area.length && !close">
+      <span class="close" v-on:click="doClose">&rarr;</span>
+      <h2 class="h2-properties">properties</h2>
+      <template v-for="property of properties">
+        <div class="property">
+          <div class="images">
+            <template v-for="image of property.images">
+              <img v-bind:src="image.src" v-bind:alt="image.src" />
+            </template>
+          </div>
 
-    <div class="property">
-      <img
-        src="https://avatars.mds.yandex.net/i?id=aba7b672a689330c910372a22922df80-5897285-images-thumbs&n=13"
-        alt=""
-      />
-      <div class="text">
-        <h3>titile</h3>
-        <p>desc</p>
-      </div>
+          <div class="text">
+            <h3>{{ property.title }}</h3>
+            <p>{{ property.description }}</p>
+          </div>
+        </div>
+      </template>
     </div>
-    <div class="property">
-      <img
-        src="https://avatars.mds.yandex.net/i?id=aba7b672a689330c910372a22922df80-5897285-images-thumbs&n=13"
-        alt=""
-      />
-      <div class="text">
-        <h3>titile</h3>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
-          iure non vel ipsum praesen
-        </p>
-      </div>
-    </div>
-  </div>
+  </transition>
 </template>
 <script>
 import { inject } from "vue";
@@ -50,7 +32,29 @@ export default {
   },
   name: "AreaProperty",
   data() {
-    return {};
+    return {
+      properties: [
+        {
+          title: "Квартира в Москве",
+          description:
+            "Молодая, красивая и одинокая квартира в москве ждет тебя!!",
+          images: [
+            {
+              src: "https://avatars.mds.yandex.net/i?id=aba7b672a689330c910372a22922df80-5897285-images-thumbs&n=13",
+              alt: "Первая фотка",
+            },
+            {
+              src: "https://avatars.mds.yandex.net/i?id=aba7b672a689330c910372a22922df80-5897285-images-thumbs&n=13",
+              alt: "Вторая фотка",
+            },
+            {
+              src: "https://avatars.mds.yandex.net/i?id=aba7b672a689330c910372a22922df80-5897285-images-thumbs&n=13",
+              alt: "Третья фотка",
+            },
+          ],
+        },
+      ],
+    };
   },
   methods: {
     closeProperty() {
@@ -73,25 +77,25 @@ export default {
   /* margin-top: 2rem; */
 }
 #properties {
-  position: absolute;
+  position: fixed;
   right: 0px;
   padding: 10px;
   /* border:3px solid black; */
   /* border-right: 2px solid rgb(224, 45, 45); */
   width: 600px;
   background-color: rgba(255, 255, 255);
-  height: 70%;
+  height: 100%;
   margin-top: -2rem;
 }
 
 .property {
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   width: 100%;
 }
 .property img {
-  width: 100px;
-  height: 100px;
+  width: 150px;
+  height: 150px;
 }
 .text {
   display: flex;
@@ -102,20 +106,43 @@ export default {
   margin-top: 30px;
 }
 
-.cross {
-  cursor: pointer; 
-  position: absolute; 
-  top: 20px; 
-  left: 10px;
+.close {
+  margin-left: -60px;
+  font-size: 2rem;
+  background-color: antiquewhite;
+  border-radius: 20px;
+  width: 25px;
+  height: 25px;
+  padding: 0.1rem 0.4rem;
+  cursor: pointer;
 }
 
-.cross:before, .cross:after {
-  content: ""; 
-  position: absolute; 
-  width: 24px; 
-  height: 4px; 
+.cross:before,
+.cross:after {
+  content: "";
+  position: absolute;
+  width: 24px;
+  height: 4px;
   background: rgb(87, 87, 87);
 }
-.cross:before {transform: rotate(45deg);}
-.cross:after {transform: rotate(-45deg);}
+.cross:before {
+  transform: rotate(45deg);
+}
+.cross:after {
+  transform: rotate(-45deg);
+}
+.slide-fade-enter-active {
+  transition: all 0.5s ease-out;
+}
+
+.slide-fade-leave-active {
+  transition: all 0.1s cubic-bezier(1, 0.5, 0.8, 1);
+}
+
+.slide-fade-enter-from,
+.slide-fade-leave-to {
+  /* right: -600px; */
+  transform: translateX(600px);
+  /* opacity: 0; */
+}
 </style>
--- src/views/Map.vue
+++ src/views/Map.vue
@@ -2,9 +2,8 @@
   <div style="display: flex">
     <h3>map</h3>
     <map-svg />
-    <transition name="slide-fade">
-      <area-property v-if="area.length && !close"></area-property>
-    </transition>
+
+    <area-property></area-property>
   </div>
 </template>
 <script>
@@ -33,19 +32,3 @@ export default {
   },
 };
 </script>
-<style>
-/* display: flex; */
-.slide-fade-enter-active {
-  transition: all 0.3s ease-out;
-}
-
-.slide-fade-leave-active {
-  transition: all 0.1s cubic-bezier(1, 0.5, 0.8, 1);
-}
-
-.slide-fade-enter-from,
-.slide-fade-leave-to {
-  transform: translateX(600px);
-  /* opacity: 0; */
-}
-</style>
